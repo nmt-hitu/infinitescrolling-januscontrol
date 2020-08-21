94d661dab9d49b23da757c4963e535ec4570fe05
Nguyễn Minh Tư <nguyenminhtuht94@gmail.com>
2020-08-21 13:45:01 +0700

    Add project files.

---
+++ InfiniteScrolling/InfiniteScrollingDialog.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace InfiniteScrolling
+{
+    partial class InfiniteScrollingDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Janus.Windows.GridEX.GridEXLayout gridEXLayout1 = new Janus.Windows.GridEX.GridEXLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InfiniteScrollingDialog));
+            this.gridNote = new Janus.Windows.GridEX.GridEX();
+            ((System.ComponentModel.ISupportInitialize)(this.gridNote)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridNote
+            // 
+            gridEXLayout1.LayoutString = resources.GetString("gridEXLayout1.LayoutString");
+            this.gridNote.DesignTimeLayout = gridEXLayout1;
+            this.gridNote.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridNote.Location = new System.Drawing.Point(0, 0);
+            this.gridNote.Name = "gridNote";
+            this.gridNote.ScrollBars = Janus.Windows.GridEX.ScrollBars.Vertical;
+            this.gridNote.Size = new System.Drawing.Size(803, 322);
+            this.gridNote.TabIndex = 0;
+            this.gridNote.Scroll += new System.EventHandler(this.GridNote_Scroll);
+            // 
+            // InfiniteScrollingDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 322);
+            this.Controls.Add(this.gridNote);
+            this.Name = "InfiniteScrollingDialog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Infinite Scrolling";
+            this.Load += new System.EventHandler(this.InfiniteScrollingDialog_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridNote)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Janus.Windows.GridEX.GridEX gridNote;
+    }
+}
+
---
+++ InfiniteScrolling/InfiniteScrollingDialog.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace InfiniteScrolling
+{
+    public partial class InfiniteScrollingDialog : Form
+    {
+        SqlConnection connection = new SqlConnection("Data Source=172.16.1.30;Initial Catalog=BOSS_Development;User Id=boss_dev; Password=GoodDev!");
+        DataTable dataTable = new DataTable();
+        DataSet dataSet;
+        SqlDataAdapter sqlDataAdapter;
+
+        int startNow = 1;
+        int endRow = 100;
+        int previousFirstDisplayedScrollingRowIndex = 0;
+
+        public InfiniteScrollingDialog()
+        {
+            InitializeComponent();
+        }
+
+        public void LoadDataView()
+        {
+            dataTable = GetDataFromTo(startNow, endRow);
+            this.gridNote.DataSource = dataTable;
+        }
+
+        private DataTable GetDataFromTo(int startRow, int endRow)
+        {
+            string commandString = @"SELECT ROW_NUMBER() OVER(ORDER BY N.CreatedDate DESC) AS RowIndex, N.Note, N.CreatedDate
+	                                INTO #TMPthi
+		                                FROM Note n (NOLOCK)
+	                                WHERE n.CreatedDate >= '1/1/2020'
+	                                ORDER BY n.CreatedDate DESC
+
+	                                SELECT *
+	                                FROM #TMP T
+	                                WHERE T.RowIndex >= {0} AND T.RowIndex <= {1}
+                                    
+                                    DROP TABLE #TMP";
+
+            commandString = string.Format(commandString, startRow, endRow);
+
+            sqlDataAdapter = new SqlDataAdapter(commandString, connection);
+            dataSet = new DataSet();
+            sqlDataAdapter.Fill(dataSet);
+             
+            return dataSet.Tables[0];
+        }
+
+        private void InfiniteScrollingDialog_Load(object sender, EventArgs e)
+        {
+            LoadDataView();
+        }
+        private void GridNote_Scroll(object sender, EventArgs e)
+        {
+            if (this.gridNote.VerticalScrollPosition > this.previousFirstDisplayedScrollingRowIndex
+                && this.gridNote.LastVisibleRow(true) == this.gridNote.RowCount - 1)
+            {
+                //Load more
+                DataTable newData = new DataTable();
+
+                int latestStartRow = (startNow * endRow) + 1;
+                int latestEndRow = (startNow + 1) * endRow;
+
+                newData = GetDataFromTo(latestStartRow, latestEndRow);
+                dataTable.Merge(newData);
+                this.gridNote.DataSource = dataTable;
+                this.gridNote.VerticalScrollPosition = latestStartRow;
+                this.previousFirstDisplayedScrollingRowIndex = this.gridNote.VerticalScrollPosition;
+
+                startNow++;
+            }
+        }
+    }
+}
